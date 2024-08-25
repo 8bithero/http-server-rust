525c71702b877ad8f9fb75414d6a57d3895375b4
Paris Kemanes <452664+8bithero@users.noreply.github.com>
2024-08-25 12:54:50 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,20 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = if request_line == "GET / HTTP/1.1" {
-        "HTTP/1.1 200 OK"
-    } else {
-        "HTTP/1.1 404 Not Found"
+    println!("{}", request_line);
+    let (status_line, contents) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", ""),
+        _ if request_line.starts_with("GET /echo") => {
+            let echo_value =
+                &request_line["GET /echo/".len()..request_line.len() - " HTTP/1.1".len()];
+            ("HTTP/1.1 200 OK", echo_value)
+        }
+        _ => ("HTTP/1.1 404 Not Found", ""),
     };
-
-    let response = format!("{status_line}\r\n\r\n");
+    let headers_text = format!(
+        "Content-Type: text/plain\r\nContent-Length: {}\r\n",
+        contents.len()
+    );
+    let response = format!("{status_line}\r\n{headers_text}\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
 }
