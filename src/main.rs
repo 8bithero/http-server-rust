ce6a3ac0b427715b93ba8f6de7214df4044f8e27
Paris Kemanes <452664+8bithero@users.noreply.github.com>
2024-08-25 22:22:08 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
-use std::thread;
+use std::path::PathBuf;
+use std::{env, fs, thread};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -23,37 +24,61 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
+    let ok_resp = "HTTP/1.1 200 OK";
+    let not_found_resp = "HTTP/1.1 404 Not Found";
+
+    let mut content_type = "text/plain";
     let buf_reader = BufReader::new(&mut stream);
     let mut lines = buf_reader.lines();
     let request_line = lines.next().unwrap().unwrap();
+
     let (status_line, contents) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "".to_owned()),
+        "GET / HTTP/1.1" => (ok_resp, Vec::new()),
         "GET /user-agent HTTP/1.1" => {
-            // let user_agent = extract_user_agent(&mut lines);
             let user_agent = lines
                 .find_map(|line| {
                     let line = line.unwrap();
-                    if line.starts_with("User-Agent:") {
-                        Some(line["User-Agent:".len()..].trim().to_string())
-                    } else {
-                        None
-                    }
+                    line.strip_prefix("User-Agent:")
+                        .map(|user_agent| user_agent.trim().to_string().into_bytes())
                 })
-                .unwrap_or_else(|| "Unknown".to_string());
-            ("HTTP/1.1 200 OK", user_agent)
+                .unwrap_or_else(|| "Unknown".to_string().into_bytes());
+            (ok_resp, user_agent)
+        }
+        _ if request_line.starts_with("GET /files/") => {
+            content_type = "application/octet-stream";
+            match request_line.strip_prefix("GET /files/") {
+                Some(file_name) => {
+                    let file_name = file_name.trim_end_matches(" HTTP/1.1");
+                    let dir = env::args().nth(2).unwrap_or_else(|| ".".to_string());
+                    let mut file_path = PathBuf::from(&dir);
+                    file_path.push(file_name);
+
+                    if file_path.exists() && file_path.is_file() {
+                        match fs::read(&file_path) {
+                            Ok(contents) => (ok_resp, contents),
+                            Err(_) => (not_found_resp, Vec::new()),
+                        }
+                    } else {
+                        (not_found_resp, Vec::new())
+                    }
+                }
+                None => (not_found_resp, Vec::new()),
+            }
         }
         _ if request_line.starts_with("GET /echo") => {
             let echo_value =
                 &request_line["GET /echo/".len()..request_line.len() - " HTTP/1.1".len()];
-            ("HTTP/1.1 200 OK", echo_value.to_owned())
+            (ok_resp, echo_value.to_owned().into_bytes())
         }
-        _ => ("HTTP/1.1 404 Not Found", "".to_owned()),
+        _ => (not_found_resp, Vec::new()),
     };
 
     let headers_text = format!(
-        "Content-Type: text/plain\r\nContent-Length: {}\r\n",
+        "Content-Type: {}\r\nContent-Length: {}\r\n",
+        content_type,
         contents.len()
     );
-    let response = format!("{status_line}\r\n{headers_text}\r\n{contents}");
+    let response = format!("{status_line}\r\n{headers_text}\r\n");
     stream.write_all(response.as_bytes()).unwrap();
+    stream.write_all(&contents).unwrap();
 }
